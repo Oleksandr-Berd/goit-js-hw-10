c4fe5d724e77e3caece91a51aa3707cebb1fb74d
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-09-02 22:05:31 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import './css/styles.css';
 
+import countryTmpl from './country.hbs';
+
+import debounce from 'lodash.debounce';
+
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  countryInfo: document.querySelector('.country-info'),
+  searchForm: document.querySelector('#search-box'),
+};
+
+refs.searchForm.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(evt) {
+  const form = evt.target.value;
+
+  fetchCountryByName(form)
+    .then(renderCountry)
+    .catch(error => console.log('error'));
+}
+
+function fetchCountryByName(countryName) {
+  return fetch(`https://restcountries.com/v3.1/name/${countryName}`).then(
+    response => {
+      return response.json();
+      console.log(response.json());
+    }
+  );
+}
+
+function renderCountry(country) {
+  const markup = countryTmpl(country);
+
+  refs.countryInfo.innerHTML = markup;
+}
