0746a16c086d25af4b2f9386a186778235880c73
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-09-04 08:33:18 +0300

    1

---
+++ src/fetchCountries.js
@@ -0,0 +1,9 @@
+const BASE_URL = `https://restcountries.com/v2`;
+
+function fetchCountry(countryName) {
+  return fetch(`${BASE_URL}/name/${countryName}`).then(response => {
+    return response.json();
+  });
+}
+
+export default { fetchCountry };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,43 @@
 import './css/styles.css';
-
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+import API from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
-
 const refs = {
   countryInfo: document.querySelector('.country-info'),
   searchForm: document.querySelector('#search-box'),
 };
 
-function fetchCountries() {
-  fetch(`https://restcountries.com/v3.1/name/usa`)
-    .then(response => {
-      return response.json();
+function renderCountryCard(country) {
+  const markUp = country
+    .map(el => {
+      return `<div class="country_card">
+        <img class="flag" src="${el.flags.svg}" alt ="${
+        el.name
+      }" width = "100" height = "80"></>
+        <h2 class = "name">Name: ${el.name}</h2>
+        <p class= "population">Population: ${el.population}</p>
+        <p class = "capital">Capital: ${el.capital}</p>
+        <p class = "language">Language: ${Object.values(el.languages).join(
+          ', '
+        )}</p>
+        </div>`;
     })
-    .then(country => {
-      console.log(country);
-    })
-    .catch(error => console.log(error));
+    .join('');
+  refs.countryInfo.insertAdjacentHTML('afterbegin', markUp);
 }
-console.log(fetchCountries());
+
+refs.searchForm.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(evt) {
+  evt.preventDefault();
+  const countryDraft = evt.target.value;
+  const country = countryDraft.trim();
+  API.fetchCountry(country).then(renderCountryCard).catch(onFetchError);
+}
+
+function onFetchError(error) {
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+}
